fc51f35ad0e231bd89495955388344c153b23baa
Shashank Kumar <2021kucp1137@iiitkota.ac.in>
2024-02-11 02:01:02 +0530

    route url change

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:4000/api/v1"
-const BASE_URL = process.env.REACT_URL_BASE_URL
+const BASE_URL = process.env.REACT_APP_BASE_URL
 
 export const Contact_api ={
     CONTACT_US:BASE_URL+"/contactus",
